23fcc6560bcab50de074bedc659af01872f90a1f
ogier_ubuntu <haomiao_li@outlook.com>
2023-04-24 15:46:08 +0800

    变更

--- src/csv2rosbag/include/localization_pose/proto2msg_sub.py
+++ src/csv2rosbag/include/localization_pose/proto2msg_sub.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
-#coding=utf-8
 import rospy
-from ..include.localization_pose import localization
-
-import numpy as py
-#导入mgs到pkg中
+# 导入mgs到pkg中
 from std_msgs.msg import String
+from localization_pb2 import LocalizationEstimate, LocalizationStatus, Uncertainty
+from google.protobuf.descriptor_pool import DescriptorPool
 
-#回调函数输入的应该是msg
-def callback(data):
-    localization = localization_pb2.LocalizationEstimate()
-    localization.ParseFromString(data.data)
-    print(localization.pose.position.x)
+import base64
+
+descriptor_pool = DescriptorPool()
+
+
+# 回调函数输入的应该是msg
+def callback(message):
+    #print(message.data)
+    message.data.debugstring()
+    print(message)
 
 def listener():
     rospy.init_node('pylistener', anonymous=True)
-    #Subscriber函数第一个参数是topic的名称，第二个参数是接受的数据类型 第三个参数是回调函数的名称
-    rospy.Subscriber('/apollo/localization/pose',String, callback)
+    # Subscriber函数第一个参数是topic的名称，第二个参数是接受的数据类型 第三个参数是回调函数的名称
+    rospy.Subscriber('/apollo/localization/pose', String, callback)
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
--- src/csv2rosbag/src/proto_sub.cpp
+++ src/csv2rosbag/src/proto_sub.cpp
@@ -1,3 +1,20 @@
 //
 // Created by ogier on 23-4-23.
 //
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <src/csv2rosbag/include/localization_pose/localization.pb.h>
+
+void callback(const std_msgs::String::ConstPtr &msg)
+{
+    ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("/apollo/localization/pose", 1000, callback);
+    ros::spin();
+    return 0;
+}
